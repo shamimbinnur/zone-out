6e3e29fd1bde6279cc06e6f49f109f60b86753b5
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-02-15 10:36:02 +0600

    Clean boilerplate code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+  
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
