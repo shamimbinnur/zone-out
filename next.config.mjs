2ed7e9516cf4152c367f220c756058980aba2f23
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-03-28 17:51:14 +0600

    Change redirect URL

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/tool/pomodoro',
+        destination: '/tool/pomo',
         permanent: false,
       },
     ];
