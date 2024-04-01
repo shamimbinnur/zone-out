0087a2e94e05b4cdcd59998b72145570f90baa18
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-04-01 23:08:30 +0600

    :)

--- components/pomo/Timer.tsx
+++ components/pomo/Timer.tsx
@@ -108,15 +108,15 @@ const Timer = () => {
                 <animated.div
                 style={resetBtnAnimationProps}
                 onClick={increaseMinutes}
-                className="bg-out-green-200 hover:rotate-90 group bg-opacity-50 h-5 w-6 cursor-pointer sm:text-lg border-out-green-200 hover:text-white transition-all duration-150 ease-in text-out-green-800 sm:hover:bg-opacity-50 rounded-l-lg flex items-center justify-center">
-                  <IoMdAdd className="rotate-90 group-hover:rotate-0 ease-out transition duration-500"/>
+                className="bg-out-green-200 sm:hover:rotate-90 group bg-opacity-50 h-5 w-6 cursor-pointer sm:text-lg border-out-green-200 sm:hover:text-white transition-all duration-150 ease-in text-out-green-800 sm:hover:bg-opacity-50 rounded-l-lg flex items-center justify-center">
+                  <IoMdAdd className="rotate-90 sm:group-hover:rotate-0 ease-out transition duration-500"/>
                 </animated.div>
 
                 <animated.div
                 style={resetBtnAnimationProps}
                 onClick={decreaseMinutes}
-                className="bg-out-green-200 hover:rotate-90 group bg-opacity-50 h-5 w-6 cursor-pointer sm:text-lg border-out-green-200 hover:text-white transition-all duration-150 ease-in text-out-green-800 sm:hover:bg-opacity-50 rounded-r-lg flex items-center justify-center">
-                  <IoMdRemove className=" group-hover:rotate-0 ease-out transition duration-500"/>
+                className="bg-out-green-200 sm:hover:rotate-90 group bg-opacity-50 h-5 w-6 cursor-pointer sm:text-lg border-out-green-200 sm:hover:text-white transition-all duration-150 ease-in text-out-green-800 sm:hover:bg-opacity-50 rounded-r-lg flex items-center justify-center">
+                  <IoMdRemove className="ease-out transition duration-500"/>
                 </animated.div>
               </div>
 
@@ -124,11 +124,10 @@ const Timer = () => {
                 <animated.div
                 style={resetBtnAnimationProps}
                 onClick={resetTimer}
-                className="bg-out-green-200 bg-opacity-50 h-5 w-8 group cursor-pointer sm:text-sm sm:hover:text-white transition-all duration-150 ease-in text-out-green-800 hover:bg-opacity-50 rounded-lg flex items-center justify-center">
-                  <GrPowerReset className="-rotate-180 group-hover:rotate-0 ease-out transition duration-500"/>
+                className="bg-out-green-200 bg-opacity-50 h-5 w-8 group cursor-pointer sm:text-sm sm:hover:text-white transition-all duration-150 ease-in text-out-green-800 sm:hover:bg-opacity-50 rounded-lg flex items-center justify-center">
+                  <GrPowerReset className="-rotate-180 sm:group-hover:rotate-0 ease-out transition duration-500"/>
                 </animated.div>
               </div>
-
             </section>
             
             <animated.div
