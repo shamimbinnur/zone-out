a17affc722333d23668e44ef012126a2799dac8e
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-04-07 16:21:06 +0600

    Update page.tsx with new content and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
+  const h1 = "Have you been zoning out lately? No worries we've got your back!"
+  const p = "Elevate your productivity with our comprehensive suite of tools. Designed to foster deep focus and prevent burnout, Zone Out maximizes efficiency for optimal task management."
+  
   return (
-    <main className="bg-stone-50 dark:bg-stone-950 h-screen p-10 flex justify-center items-center text-stone-950 dark:text-stone-50">
-      <h1 className="text-4xl font-bold text-center">Have you been zoning out lately? No worries we&apos;ve got your back!</h1>
+    <main className="bg-stone-50 dark:bg-stone-950 text-stone-950 dark:text-stone-50">
+      <section className="text-center gap-y-6 h-screen p-10 flex flex-col justify-center items-center">
+        <h1 className="text-4xl max-w-6xl font-bold text-center">{h1}</h1>
+        <p className="text-lg max-w-5xl">{p}</p>
+      </section>
     </main>
   );
 }
