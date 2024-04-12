eaac48f4b8ebc2b987f389e6406f0194f205d06e
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-04-12 23:37:56 +0600

    Update MDXsection styles in components/common/MDXsection.tsx

--- components/common/MDXsection.tsx
+++ components/common/MDXsection.tsx
@@ -18,8 +18,7 @@ const MDXsection:FC<MDXsectionProps> = ({
       prose-h1:text-4xl prose-h1:font-semibold prose-h1:tracking-tighter prose-h1:leading-tight prose-h1:mb-16
       prose-h2:text-3xl prose-h2:font-semibold prose-h2:tracking-tighter prose-h2:leading-tight
       prose-p:text-lg prose-p:font-normal prose-p:tracking-tight prose-p:leading-normal prose-p:my-6
-      prose-ol:text-xl prose-ol:font-normal prose-ol:tracking-tighter prose-ol:leading-normal prose-ol:my-8
-      prose-li:text-xl prose-li:font-normal prose-li:tracking-tighter prose-li:leading-normal prose-li:my-8
+      prose-li:text-lg prose-li:font-normal prose-li:tracking-tight prose-li:leading-normal prose-li:my-6
       ">
       <MDXRemote source={content} />
     </section>
