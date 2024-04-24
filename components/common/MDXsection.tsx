f7a405c24db65169e078847b0a8d85abf9150b14
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-04-24 18:57:44 +0600

    Update MDXsection component styles

--- components/common/MDXsection.tsx
+++ components/common/MDXsection.tsx
@@ -11,10 +11,10 @@ const MDXsection:FC<MDXsectionProps> = ({
   content
 }) => {
   return (
-    <div className="bg-stone-50 dark:bg-stone-950">
+    <div className="bg-stone-50">
       <section
         className="
-        text-stone-950 dark:text-stone-50
+        text-stone-950
           mx-auto max-w-5xl px-4 pt-16 sm:pt-32 pb-14 prose
           prose-h1:text-4xl prose-h1:font-semibold prose-h1:tracking-normal prose-h1:leading-tight prose-h1:mb-16
           prose-h2:text-3xl prose-h2:font-semibold prose-h2:tracking-tight prose-h2:leading-tight
