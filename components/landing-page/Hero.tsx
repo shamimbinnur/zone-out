5dd24e8874ad99de553c489b8265bbf568d9173c
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-05-07 18:51:49 +0600

    refactor: Improve Pomo component image container and button styling

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
     <section className="flex flex-col items-center pt-10 md:pt-60">
-      <h1 className="text-left 1088:text-center text-7xl font-extralight max-w-6xl tracking-tightest">{h1}</h1>
-      <p className="text-left py-8 1088:text-center text-4xl font-archivo md:max-w-5xl font-extralight">{p}</p>
+      <h1 className="text-left 1088:text-center text-5xl font-extralight lg:leading-[1.2] max-w-3xl tracking-tightest">{h1}</h1>
+      <p className="text-left py-8 1088:text-center text-lg font-archivo md:max-w-2xl font-extralight">{p}</p>
       <div className="max-w-[1080px] mb-36 lg:mb-60 font-bold h-1 ">. <span className="text-red-500">. </span>.</div>
     </section>
   )
--- components/landing-page/Pomo.tsx
+++ components/landing-page/Pomo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Pomo = () => {
   return (
-    <div className="border rounded-md max-w-[780px] border-gray-100 border-opacity-20 p-6 mx-auto mt-16 mb-16">
+    <div className="border rounded-md max-w-[780px] border-gray-100 border-opacity-20 mx-auto mt-8 mb-16">
       <div className="overflow-hidden rounded-xl border-[1.5px] border-slate-400  mb-4">
         <Image
           alt="Online Pomodoro timer"
@@ -16,7 +16,7 @@ const Pomo = () => {
       </div>
 
       <h1 className="text-2xl font-medium pt-2 text-[#191919]">Pomodoro Timer</h1>
-      <p className="font-archivo pb-8 text-[#191919]">Introducing our sleek online Pomodoro timer, meticulously crafted for ultimate user-friendliness. Designed to seamlessly enhance your focus, it offers a refined experience to keep you on track with your tasks.</p>
+      <p className="font-archivo font-light pb-8 text-[#191919]">Introducing our sleek online Pomodoro timer, meticulously crafted for ultimate user-friendliness. Designed to seamlessly enhance your focus, it offers a refined experience to keep you on track with your tasks.</p>
       
       <Link href="/pomo">
         <button className="bg-gray-100 shadow-sm font-archivo text-[#191919] border font bold w-full rounded-lg px-4 py-2">Try now</button>
