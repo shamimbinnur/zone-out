e4426ed6803e075f05f3ac5ffb96608af662ae14
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-05-29 21:34:35 +0600

    add github star button.

---
+++ components/common/GitHubStartButton.tsx
@@ -0,0 +1,14 @@
+const GitHubStartButton = () => {
+  return (
+    <div>
+      <iframe
+        src="https://ghbtns.com/github-btn.html?user=shamimbinnur&repo=zone-out&type=star&count=true"
+        width="75"
+        height="20"
+        title="GitHub">
+      </iframe>
+    </div>
+  )
+}
+
+export default GitHubStartButton
--- components/pomo/Menu.tsx
+++ components/pomo/Menu.tsx
@@ -1,8 +1,10 @@
+import GitHubStartButton from '../common/GitHubStartButton'
 import InfoPopover from './InfoDropDown'
 
 const Menu = () => {
   return (
-    <div className="absolute right-0 flex justify-end px-8 py-8">
+    <div className="absolute flex items-center w-full justify-between px-8 py-8">
+      <GitHubStartButton  />
       <InfoPopover />
     </div>
   )
