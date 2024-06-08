c2db30c9748f83e07394d13c87ee03c1ba0b995a
Shamim Bin Nur <sbnsbn00@gmail.com>
2024-06-08 20:55:58 +0600

    feat: add query parameter to scroll to pomodoro content sections

--- components/pomo/ScrollDownLink.tsx
+++ components/pomo/ScrollDownLink.tsx
@@ -1,8 +1,17 @@
 "use client"
 
+import { useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 import { FaInfoCircle } from "react-icons/fa"
 
 const ScrollDownLink = () => {
+  const searchParams = useSearchParams()
+  const requiredContent = searchParams.get('section') === "what_is_pomodoro"
+  
+  useEffect(() => {
+    requiredContent ? handleInfoClick() : null
+  }, [requiredContent])
+  
   const handleInfoClick = () => {
     if (typeof window === "undefined") return
     window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
